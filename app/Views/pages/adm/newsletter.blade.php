e3a52eb01262a47186e96e4f0bb15da2bbf2c89f
LarsSluijter <lhasluij@avans.nl>
2016-04-22 15:53:07 +0200

    Added searching for senden newsletter files

--- app/Views/pages/adm/newsletter.blade.php
+++ app/Views/pages/adm/newsletter.blade.php
@@ -34,7 +34,14 @@
                 </div>
                 <div class="col-lg-6">
                     <h4>Eerder verstuurde bestanden:</h4>
-                    <table class="table table-striped table-hover">
+                    <form>
+                        <div class="form-group label-placeholder is-empty">
+                            <label for="search" class="control-label">Zoeken</label>
+                            <input type="text" autocomplete="off" class="form-control" id="search">
+                        </div>
+
+                    </form>
+                    <table id="table" class="table table-striped table-hover">
                         <tbody>
                         @foreach(File::allFiles('newsletter') as $file)
                             <tr>
@@ -48,4 +55,5 @@
 
         </div>
     </form>
+    <script src="{{url("js/newsletter.js")}}"></script>
 @endsection
