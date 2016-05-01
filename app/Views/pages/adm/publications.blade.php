69c142665585acb9c47224edac244eeaefdb94ab
mdnapo <md.napo@student.avans.nl>
2016-05-02 01:53:36 +0200

    Minor code clean up part II

--- app/Views/pages/adm/publications.blade.php
+++ app/Views/pages/adm/publications.blade.php
@@ -23,14 +23,12 @@
                     <a href="{{ url('/admin/media/' . $publication->id) }}" class="glyphicon glyphicon-file plain_link"></a>
                 </td>
                 <td>
-                    <a onclick="delete_publication('{{ $publication->id }}')" class="glyphicon glyphicon-remove plain_link"></a>
+                    <a id="{{ $publication->id }}" class="glyphicon glyphicon-remove plain_link delete_publication"></a>
                 </td>
             </tr>
         @endforeach
         </tbody>
     </table>
-
-    <form id="delete" method="POST"></form>
 
     <div class="btn btn-raised">
         <a href="{{ url('/admin/media/0') }}">Toevoegen</a>
@@ -38,18 +36,22 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
     <script>
-        function delete_publication(id){
-            bootbox.confirm('Weet u zeker dat u deze publicatie wilt verwijderen?', function(answer){
-                if(answer === true){
-                    var form = $('#delete')[0];
-                    form.action  = '{{ url('/admin/media/delete') }}/' + id;
-                    var token = document.createElement('input');
-                    token.name = '_token';
-                    token.value = '{{ csrf_token() }}';
-                    form.appendChild(token);
-                    form.submit();
-                }
+        $(document).ready(function(){
+            $('.delete_publication').click(function(){
+                var id = $(this).attr('id');
+                bootbox.confirm('Weet u zeker dat u deze publicatie wilt verwijderen?', function(answer){
+                    if(answer === true){
+                        var form = document.createElement('form');
+                        form.method = 'POST';
+                        form.action  = '{{ url('/admin/media/delete') }}/' + id;
+                        var token = document.createElement('input');
+                        token.name = '_token';
+                        token.value = '{{ csrf_token() }}';
+                        form.appendChild(token);
+                        form.submit();
+                    }
+                });
             });
-        }
+        });
     </script>
 @endsection
