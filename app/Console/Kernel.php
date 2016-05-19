64c1a172a4d2de7210eebb25b0e8a9d57c8f5a73
Dajo Hofman <d.hofman@dj33.nl>
2016-05-19 21:24:58 +0200

    Basic mail command structure

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,13 @@
 
 namespace App\Console;
 
+use App\Contact;
+use app\Http\Controllers\Admin\AdminController;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Donation;
 
-class Kernel extends ConsoleKernel
-{
+class Kernel extends ConsoleKernel {
     /**
      * The Artisan commands provided by your application.
      *
@@ -19,12 +21,24 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
-    {
+    protected function schedule(Schedule $schedule) {
         // $schedule->command('inspire')
         //          ->hourly();
+
+        $schedule->call(function () {
+            AdminController::donationMail();
+        })->when(function () {
+            if (file_exists('./Storage/mailupdate.yaml'))
+                yaml_parse_file('./Storage/mailupdate.yaml');
+            if (Donation::where('approved', 0)->count() > 0)
+                return true;
+            else if (isset($contacts) && (Contact::all()->count() > $contacts))
+                return true;
+            else
+                return false;
+        });
     }
 }
