fde8014e39c7c6a567b76eccefd7d3327695b867
Jeroen Vijgen <jjvijgen@gmail.com>
2016-05-27 09:25:16 +0200

    Added some more test controllers

---
+++ tests/SettingControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class SettingControllerTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
