80395a6a6deb7a2593de1142242546f1ec261632
Jeroen Vijgen <jjvijgen@gmail.com>
2016-05-30 22:36:34 +0200

    Removed TinyMCE

--- app/Views/pages/contact.blade.php
+++ app/Views/pages/contact.blade.php
@@ -44,6 +44,5 @@ if($text == null){
 @stop
 
 @section('footer')
-    <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
-    <script>tinymce.init({ selector: 'textarea' })</script>
+    
 @stop
