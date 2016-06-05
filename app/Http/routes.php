163d8ab968fe497ed4cbdb0e965f591a08718d0d
mdnapo <md.napo@student.avans.nl>
2016-06-06 01:02:11 +0200

    Finished new media page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -35,7 +35,7 @@ Route::group(['middleware' => ['web']], function () {
     Route::get('zoeken-in-media', 'MediaController@search');
 
     Route::get('/reactiepagina/{id}', ['uses' => 'CommentController@comments'])->where('id', '([0-9]+)');
-    Route::post('/reactiepagina/comment', ['uses' => 'CommentController@comment'])->where('id', '([0-9]+)');
+    Route::post('/comment', ['uses' => 'CommentController@comment'])->where('id', '([0-9]+)');
 
     Route::get('contact', 'ContactController@index');
     Route::post('contact', 'ContactController@insertIntoDb');
--- app/Views/pages/media-view.blade.php
+++ app/Views/pages/media-view.blade.php
@@ -8,25 +8,39 @@
         </div>
         <div class="col-xs-12">
             <h1>{{ $publication->source }}</h1>
-            <div>
-                {!! $publication->article !!}
-            </div>
+            <div>{!! $publication->article !!}</div>
         </div>
+        @if($publication->video)
+            <div class="col-xs-12 top_buffer">{!! $publication->video !!}</div>
+        @endif
         <div class="col-xs-12 top_buffer">
-            @if(count($publication->comments) > 0)
+            @if(count($publication->approvedComments) > 0)
                 <h1>Reacties</h1>
                 <div class="scroller bottom_buffer">
-                    @foreach($publication->comments as $comment)
-                        <div>{{ $comment->reactie }}</div>
-                        <div>{{ $comment->reactie }}</div>
-                        <div>{{ $comment->reactie }}</div>
-                        <div>{{ $comment->reactie }}</div>
-                    @endforeach
+                    <div class="comment">
+                        @foreach($publication->approvedComments as $comment)
+                            @if($comment->geaccepteerd == 1)
+                                <h4>{{ $comment->naam }}</h4>
+                                <div>{{ $comment->reactie }}</div>
+                            @endif
+                        @endforeach
+                    </div>
                 </div>
             @else
                 <h2>Nog geen reacties</h2>
-
             @endif
+        </div>
+        <div class="col-xs-12">
+            <h1 class="entry-title">Een reactie plaatsen</h1>
+            <form method="POST" action="{{ url('comment') }}" class="well well-lg" >
+                <input type="hidden" name="_token" value="{{ csrf_token() }}" >
+                <input type="hidden" name="publication_id" value="{{ $publication->id }}" />
+                <label class="control-label" for="email">Naam</label>
+                <input type="name" class="form-control" name="name">
+                <label class="control-label" for="bericht">Reactie</label>
+                <textarea type="comment" class="form-control" name="comment"></textarea>
+                <input type="submit" class="btn btn-primary" value="Versturen"/>
+            </form>
         </div>
     </div>
 @stop
@@ -36,12 +50,13 @@
     <script src="{{ asset('js/jquery.highlight.js') }}"></script>
     <script>
         $(document).ready(function () {
-//            $('.collapse.publication').show();
-//            $('img').addClass('img-responsive');
-//            fluidvids.init({
-//                selector: ['iframe'],
-//                players: ['www.youtube.com', 'player.vimeo.com'] // players to support
-//            })
+            $('iframe').addClass('embed-responsive-item');
+            $('img').addClass('img-responsive');
+            fluidvids.init({
+                selector: ['iframe'],
+                players: ['www.youtube.com', 'player.vimeo.com'] // players to support
+            })
+
         });
     </script>
 @stop
